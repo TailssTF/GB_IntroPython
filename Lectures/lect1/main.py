82f59eda6dff6adaeb94485e38a6be3624bdf642
TailssTF <tailss5@mail.ru>
2023-05-26 17:40:07 +0700

    moving

---
+++ Lectures/lect1/main.py
@@ -0,0 +1,18 @@
+
+a = 5
+b = 5.89
+c = 'hello'
+
+print(f"{a} - {b} - {c}")
+print("{} - {} - {}".format(a, b, c))
+
+print("Введите первое число:")
+a = int(input())
+
+b = int(input('Введите второе число: '))
+
+print(a, ' + ', b, ' = ', a + b)
+
+a = 5.896546
+b = 3.464564
+print(round(a*b, 3))
